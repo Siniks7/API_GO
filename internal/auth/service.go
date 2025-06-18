41102becf3fca37f0df33e6223e5e4eebe2480af
Nikolay <siniks7@yandex.ru>
2025-06-18 16:45:08 +0700

    Логин пользователя

--- internal/auth/service.go
+++ internal/auth/service.go
@@ -35,3 +35,15 @@ func (service *AuthService) Register(email, password, name string) (string, erro
 	}
 	return user.Email, nil
 }
+
+func (service *AuthService) Login(email, password string) (string, error) {
+	existedUser, _ := service.UserRepository.FindByEmail(email)
+	if existedUser == nil {
+		return "", errors.New(ErrWrongCredetials)
+	}
+	err := bcrypt.CompareHashAndPassword([]byte(existedUser.Password), []byte(password))
+	if err != nil {
+		return "", errors.New(ErrWrongCredetials)
+	}
+	return existedUser.Email, nil
+}
