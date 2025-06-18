12fa81c1eebeb21ceb230216a8804565dad50899
Nikolay <siniks7@yandex.ru>
2025-06-19 00:00:28 +0700

    Один ко многим (stats and link)

--- migrations/auto.go
+++ migrations/auto.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"api/internal/link"
+	"api/internal/stat"
 	"api/internal/user"
 	"os"
 
@@ -19,5 +20,5 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	db.AutoMigrate(&link.Link{}, &user.User{})
+	db.AutoMigrate(&link.Link{}, &user.User{}, &stat.Stat{})
 }
